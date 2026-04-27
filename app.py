969aa8352b2a4fab90e7b91e476005d5dde87508
Archana <archu40neerati@gmail.com>
2026-04-27 09:53:24 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = FLask(__name__)
+
+@app.route("/")
+def home():
+    return "Monitoring Devops Project"
+
+if __name__=="__main__":
+   app.run(host="0.0.0.0",port=5000)
